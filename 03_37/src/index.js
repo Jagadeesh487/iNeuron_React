3ddc404009d243d21e24283e885ee47456ca8f7d
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-03-05 22:59:38 +0530

    shoes

--- 03_37/src/index.js
+++ 03_37/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Forth(){
+  return <div>
+    <h1>TYPES OF SHOES</h1>
+    <Photo/>
+    <Name/>
+  </div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Photo =() => <img src="https://rukminim1.flixcart.com/image/200/200/kit6hzk0-0/toy-sport/k/q/l/inline-skates-abec-7-unisex-indoor-and-outdoor-adjustable-size-original-imafygsgdruhej5m.jpeg?q=70" alt="no iamge"></img>
+
+const Name =() => <h3>AR Jouiet Mansion Blue Skatting Skates and Skateboard</h3>
+
+ReactDOM.render(<Forth/>,document.getElementById('root'))
